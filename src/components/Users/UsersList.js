ae0d4617fb9fd204590217c5a3e2e033e84022d9
Sardor Madaminov <sardor0968@gmail.com>
2022-11-03 21:50:35 +0900

    minor changes: added

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "../../UI/Card";
 import classes from "./UsersList.module.css";
 
-const UsersList = () => {
+const UsersList = (props) => {
   return (
     <Card className={classes.users}>
       {" "}
